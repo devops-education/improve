bcb4c435bbf4b64592bdb757938b20a86f0c06e6
Marcelo Romulo Fernandes <persapiens@gmail.com>
2023-05-10 22:30:33 -0300

    initial implementation of resume. #WIP

--- src/main/java/org/persapiens/crde/persistence/RecommendationFeedbackRepository.java
+++ src/main/java/org/persapiens/crde/persistence/RecommendationFeedbackRepository.java
@@ -11,4 +11,12 @@ public interface RecommendationFeedbackRepository extends CrudRepository<Recomme
     List<RecommendationFeedback> findByUsername(String username);
     
     List<RecommendationFeedback> findByRecommendationInAndUsername(Collection<Recommendation> recommendations, String username);
+    
+    /**
+     * lista de recomendacoes por pretendo usar e nao utilizava.
+     * ordenar por nao conhecia.
+     * @param username usuario
+     * @return lista de recomendacoes
+     */
+    List<RecommendationFeedback> findByUsernameAndUsedAlreadyIsFalseAndWillUseIsTrueOrderByKnownAsc(String username);
 }
