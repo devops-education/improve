893fafb9ceaff24f4987de7d21ce0ad6b9145b20
Marcelo Romulo Fernandes <persapiens@gmail.com>
2023-07-22 10:36:10 -0300

    upgrade joinfaces to 5.1.2 and spring boot to 3.1.2

--- src/main/java/org/persapiens/crde/SecurityConfig.java
+++ src/main/java/org/persapiens/crde/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +14,9 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @SuppressFBWarnings("SPRING_CSRF_PROTECTION_DISABLED")
 @Configuration
@@ -24,28 +28,37 @@ public class SecurityConfig {
      * Configure security.
      */
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) {
+    public SecurityFilterChain configure(HttpSecurity http, MvcRequestMatcher.Builder mvc) {
         try {
-            http.csrf((t) -> {t
-                .disable();});
-            http.authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/login.faces").permitAll()
-                .requestMatchers("/jakarta.faces.resource/**").permitAll()
+            http = http.csrf((t) -> {
+                t.disable();
+            });
+            http = http.authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers(mvc.pattern("/")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/login.faces")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/jakarta.faces.resource/**")).permitAll()
                 .anyRequest().authenticated());
-            http.formLogin((formLogin) -> {formLogin
+            http = http.formLogin((formLogin) -> {formLogin
                 .loginPage("/login.faces")
                 .permitAll()
                 .failureUrl("/login.faces?error=true")
-                .defaultSuccessUrl("/");});
-            http.logout((logout) -> {logout
+                .defaultSuccessUrl("/");
+            });
+            http = http.logout((logout) -> {logout
                 .logoutSuccessUrl("/login.faces")
-                .deleteCookies("JSESSIONID");});
+                .deleteCookies("JSESSIONID");
+            });
 
             return http.build();
         } catch (Exception ex) {
             throw new BeanCreationException("Wrong spring security configuration", ex);
         }
+    }
+
+    @Scope("prototype")
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
     }
 
     /**
