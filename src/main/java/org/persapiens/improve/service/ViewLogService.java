3336fae2bacd7b6342f28cfbbb6b898865ed7f9c
Marcelo Romulo Fernandes <persapiens@gmail.com>
2023-08-16 11:49:49 -0300

    initial view log implementation. view log domain, repository and service. missing view actions.

---
+++ src/main/java/org/persapiens/improve/service/ViewLogService.java
@@ -0,0 +1,24 @@
+package org.persapiens.improve.service;
+
+import lombok.Getter;
+import org.persapiens.improve.domain.ViewLog;
+import org.persapiens.improve.persistence.ViewLogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ViewLogService extends RepositoryCrudService <ViewLog, Long> {
+
+    @Getter
+    @Autowired
+    private ViewLogRepository repository;
+
+    @Override
+    public void save(ViewLog bean) {
+        if (bean.getId() == null) {
+            bean.insert();
+        }
+        super.save(bean);
+    }
+    
+}
