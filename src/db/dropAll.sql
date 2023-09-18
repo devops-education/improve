b22e0824a0ff9141ca7b2f4dbacd9af1fc4b009f
Marcelo Romulo Fernandes <persapiens@gmail.com>
2023-09-18 11:58:21 -0300

    updating backup create drop all script

---
+++ src/db/dropAll.sql
@@ -0,0 +1,4 @@
+drop table challenge, challenge_feedback, challenge_interview, challenge_recommendation_conflict, challenge_tag, interview cascade;
+drop table recommendation, recommendation_feedback, recommendation_interview, recommendation_tag, recommendations_conflict cascade;
+drop table link, authorities, tag, teaching_method, teaching_method_feedback, teaching_method_link, users, view_log cascade;
+drop sequence seq_authority, seq_challenge_feedback, seq_recommendation_feedback, seq_teaching_method_feedback, seq_user, seq_view_log;
