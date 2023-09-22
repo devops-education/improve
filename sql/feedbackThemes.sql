83ec2dfa581dfb3a3e152ae12ddbdb0977188869
Marcelo Romulo Fernandes <persapiens@gmail.com>
2023-09-22 15:03:07 -0300

    salvando sqls na pasta sql.

--- sql/feedbackThemes.sql
+++ sql/feedbackThemes.sql
@@ -2,4 +2,5 @@ select distinct(c.theme), cf.user_id, u.username
 from challenge_feedback cf, challenge c, users u
 where cf.challenge_id = c.id
 and cf.user_id = u.id
+and cf.user_id not in (1,3,23,25,25,26)
 order by u.username
