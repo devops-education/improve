2c57dd0252aa4f9705c92a26698064fd7e7c1e2d
Marcelo Romulo Fernandes <persapiens@gmail.com>
2023-09-23 11:18:52 -0300

    melhorando a consulta da quantidade feedback com comentarios.

--- sql/feedbackChallenge.sql
+++ sql/feedbackChallenge.sql
@@ -9,9 +9,14 @@ group by u.username, c.theme
 order by 1, quantidade desc
 
 
--- ADICIONAR COMENTARIO
-select f.user_id, count(distinct f.id) as quantidade_challenge_comment
-from challenge_feedback f
-where length(f.will_mitigate_comment) > 0 and will_mitigate_comment is not null
+-- listando a quantidade de feedback de desafios com comentario 
+-- agrupados por usuario e "serão mitigados"
+-- considerando apenas os usuarios participantes do estudo de caso
+select f.user_id, max(u.username), f.will_mitigate, count(distinct f.id) as quantidade_challenge_comment
+from challenge_feedback f, users u
+where length(f.will_mitigate_comment) > 0 is not null
 and f.user_id not in (1,3,23,25,25,26)
-group by f.user_id;
+and f.user_id = u.id
+group by f.user_id, f.will_mitigate
+order by 2;
+
