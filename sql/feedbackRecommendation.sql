0f6cc77c2a4f9d2e15e65e43da8ca9659d6ca880
Marcelo Romulo Fernandes <persapiens@gmail.com>
2023-10-20 11:37:12 -0300

    improve sqls to gather better results.

--- sql/feedbackRecommendation.sql
+++ sql/feedbackRecommendation.sql
@@ -4,114 +4,103 @@ select max(f.user_id) as user_id, u.username, c.theme, count(f.id) as quantidade
 from recommendation_feedback f, recommendation c, users u
 where f.recommendation_id = c.id
 and f.user_id = u.id
-and f.user_id not in (1,3,23,25,25,26)
+and f.user_id in (27,28,29,30,31,32,35,36,37,40,41)
 group by u.username, c.theme
 order by 1, quantidade desc
 
 
--- listando a quantidade de feedback de recommendacao com comentario "ja usaram"
--- agrupados por usuario e "ja usuram"
--- considerando apenas os usuarios participantes do estudo de caso
-select f.user_id, min(u.username), f.used_already,
-	count(distinct f.id) as quantidade_recommendation_comment
-from recommendation_feedback f, users u
-where length(f.used_already_comment) > 0 
-and f.user_id not in (1,3,23,25,25,26)
-and f.user_id = u.id
-group by f.user_id, f.used_already
-order by 2;
-
-
--- listando a quantidade de feedback de recommendacao com comentario "pretende usar"
--- agrupados por usuario e "pretende usar"
--- considerando apenas os usuarios participantes do estudo de caso
-select f.user_id, min(u.username), f.will_use,
-	count(distinct f.id) as quantidade_recommendation_comment
-from recommendation_feedback f, users u
-where length(f.will_use_comment) > 0 
-and f.user_id not in (1,3,23,25,25,26)
-and f.user_id = u.id
-group by f.user_id, f.will_use
-order by 2;
-
-
--- listando as recomendações mais comentados que FORAM utilizadas
+-- listando as recomendações que FORAM utilizadas
 -- limitando a quantidade de feedback em 4
-select rf.recommendation_id, max(r.theme), count(rf.id) as quantidade, max(r.main_idea)
+select rf.recommendation_id, count(rf.id) as quantidade, max(r.main_idea), max(r.theme)
 from recommendation_feedback rf, recommendation r
 where rf.recommendation_id = r.id
-and rf.user_id not in (1,3,23,25,25,26)
 and rf.used_already = true
+and rf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
 group by rf.recommendation_id
 having count(rf.id) >= 4
 order by quantidade desc
 
 
--- listando as recomendações mais comentados que NÃO FORAM utilizadas e PRETENDE utilizar
+-- listando as recomendações que NÃO FORAM utilizadas e PRETENDE utilizar
 -- limitando a quantidade de feedback em 4
-select rf.recommendation_id, max(r.theme), count(rf.id) as quantidade, max(r.main_idea)
+select rf.recommendation_id, count(rf.id) as quantidade, max(r.main_idea), max(r.theme)
 from recommendation_feedback rf, recommendation r
 where rf.recommendation_id = r.id
-and rf.user_id not in (1,3,23,25,25,26)
 and rf.used_already = false
 and rf.will_use = true
+and rf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
 group by rf.recommendation_id
 having count(rf.id) >= 4
 order by quantidade desc
 
 
--- listando as recomendações mais comentados que NÃO FORAM utilizadas e NÃO PRETENDE utilizar
+-- listando as recomendações que NÃO FORAM utilizadas e NÃO PRETENDE utilizar
 -- limitando a quantidade de feedback em 4
-select rf.recommendation_id, max(r.theme), count(rf.id) as quantidade, max(r.main_idea)
+select rf.recommendation_id, count(rf.id) as quantidade, max(r.main_idea), max(r.theme)
 from recommendation_feedback rf, recommendation r
 where rf.recommendation_id = r.id
-and rf.user_id not in (1,3,23,25,25,26)
 and rf.used_already = false
 and rf.will_use = false
+and rf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
 group by rf.recommendation_id
 having count(rf.id) >= 4
 order by quantidade desc
 
 
--- listando as recomendações que ao mesmo tempo FORAM E NÃO FORAM UTILIZADAS
--- limitando a quantidade de feedback em 4
+-- listando as recomendações que ao mesmo tempo FORAM + SERÃO COM NÃO SERÃO UTILIZADAS
+-- limitando a quantidade de feedback em 3
 -- PODE INDICAR MÁ ESCRITA DA RECOMENDAÇÃO? OU SERIA ALGO MAIS?
-select recommendation_id, min(quantidade), max(quantidade), min(main_idea), max(theme)
-from (
+SELECT WONT_USE.recommendation_id, WONT_USE.wont_use_amount, USED_ALREADY_WILL_USE.used_already_will_use_amount, WONT_USE.main_idea, WONT_USE.theme
+FROM 
+(
+    -- selecionando a quantidade de nao uso das recomendacoes
+    -- filtrando por 3
 	select rf.recommendation_id as recommendation_id, 
 		max(r.theme) as theme, 
-		rf.used_already as used_already,
-		count(rf.id) as quantidade,
+		count(rf.id) as wont_use_amount,
 		max(r.main_idea) as main_idea
 	from recommendation_feedback rf, recommendation r
 	where rf.recommendation_id = r.id
-        and rf.user_id not in (1,3,23,25,25,26)
-	group by rf.recommendation_id, rf.used_already
-	having count(rf.id) >= 4
-)
-group by recommendation_id
-having count(recommendation_id) = 2
-order by 1 
-
-
-
--- listando as recomendações que não foram utilizadas e ao mesmo tempo SERÃO E NÃO SERÃO UTILIZADAS
--- limitando a quantidade de feedback em 2
--- PODE INDICAR MÁ ESCRITA DA RECOMENDAÇÃO? OU SERIA ALGO MAIS?
-select recommendation_id, min(quantidade), max(quantidade), min(main_idea), max(theme)
-from (
-	select rf.recommendation_id as recommendation_id, 
-		max(r.theme) as theme, 
-		rf.will_use as will_use,
-		count(rf.id) as quantidade,
-		max(r.main_idea) as main_idea
-	from recommendation_feedback rf, recommendation r
-	where rf.recommendation_id = r.id
-        and rf.user_id not in (1,3,23,25,25,26)
-		and rf.used_already = false
-	group by rf.recommendation_id, rf.will_use
+			and rf.used_already = false
+			and rf.will_use = false
+            and rf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
+	group by rf.recommendation_id
 	having count(rf.id) >= 3
-)
-group by recommendation_id
-having count(recommendation_id) >= 2
-order by 1 
+) WONT_USE
+
+JOIN 
+(
+    -- selecionando a quantidade de recomendacoes ja usadas adicionadas com as que serao usadas
+    -- filtrando por 3
+    select coalesce(USED_ALREADY.recommendation_id, WILL_USE.recommendation_id) as recommendation_id, 
+           coalesce(USED_ALREADY.used_already_amount,0) + coalesce(WILL_USE.will_use_amount, 0) as used_already_will_use_amount
+    from (
+        -- selecionando a quantidade de recomendacoes ja usadas
+        (select rf.recommendation_id as recommendation_id, 
+            count(rf.id) as used_already_amount
+        from recommendation_feedback rf, recommendation r
+        where rf.recommendation_id = r.id
+                and rf.used_already = true
+                and rf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
+        group by rf.recommendation_id) USED_ALREADY
+        
+        full join
+        
+        -- selecionando a quantidade de recomendacoes a serem usadas usadas
+        (select rf.recommendation_id as recommendation_id, 
+            count(rf.id) as will_use_amount
+        from recommendation_feedback rf, recommendation r
+        where rf.recommendation_id = r.id
+                and rf.used_already = false
+                and rf.will_use = true
+                and rf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
+        group by rf.recommendation_id) WILL_USE
+        
+        on USED_ALREADY.recommendation_id = WILL_USE.recommendation_id
+    )
+    where coalesce(USED_ALREADY.used_already_amount,0) + coalesce(WILL_USE.will_use_amount, 0) >= 3
+) USED_ALREADY_WILL_USE
+
+ON WONT_USE.recommendation_id= USED_ALREADY_WILL_USE.recommendation_id
+
+ORDER BY 2 DESC, 3 DESC
