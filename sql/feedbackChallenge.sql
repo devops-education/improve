b99b5ac7a12f97cdf938d05d16083dee3db377d6
Marcelo Romulo Fernandes <persapiens@gmail.com>
2023-10-20 11:37:12 -0300

    improve sqls to gather better results.

--- sql/feedbackChallenge.sql
+++ sql/feedbackChallenge.sql
@@ -4,30 +4,18 @@ select max(f.user_id) as user_id, u.username, c.theme, count(f.id) as quantidade
 from challenge_feedback f, challenge c, users u
 where f.challenge_id = c.id
 and f.user_id = u.id
-and f.user_id not in (1,3,23,25,25,26)
+and f.user_id in (27,28,29,30,31,32,35,36,37,40,41)
 group by u.username, c.theme
 order by 1, quantidade desc
 
 
--- listando a quantidade de feedback de desafios com comentario 
--- agrupados por usuario e "serão mitigados"
--- considerando apenas os usuarios participantes do estudo de caso
-select f.user_id, max(u.username), f.will_mitigate, count(distinct f.id) as quantidade_challenge_comment
-from challenge_feedback f, users u
-where length(f.will_mitigate_comment) > 0 is not null
-and f.user_id not in (1,3,23,25,25,26)
-and f.user_id = u.id
-group by f.user_id, f.will_mitigate
-order by 2;
-
-
 -- listando os desafios mais comentados que SERÃO mitigados
 -- limitando a quantidade de feedback em 4
-select cf.challenge_id, max(c.theme), count(cf.id) as quantidade, max(c.main_idea)
+select cf.challenge_id, count(cf.id) as quantidade, max(c.main_idea), max(c.theme)
 from challenge_feedback cf, challenge c
 where cf.challenge_id = c.id
-and cf.user_id not in (1,3,23,25,25,26)
 and cf.will_mitigate = true
+and cf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
 group by cf.challenge_id
 having count(cf.id) >= 4
 order by quantidade desc
@@ -35,11 +23,11 @@ order by quantidade desc
 
 -- listando os desafios mais comentados que NÃO vao ser mitigados
 -- limitando a quantidade de feedback em 3
-select cf.challenge_id, max(c.theme), count(cf.id) as quantidade, max(c.main_idea)
+select cf.challenge_id, count(cf.id) as quantidade, max(c.main_idea), max(c.theme)
 from challenge_feedback cf, challenge c
 where cf.challenge_id = c.id
-and cf.user_id not in (1,3,23,25,25,26)
 and cf.will_mitigate = false
+and cf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
 group by cf.challenge_id
 having count(cf.id) >= 3
 order by quantidade desc
@@ -48,22 +36,35 @@ order by quantidade desc
 -- listando as desafios que serao ao mesmo tempo mitigados e nao mitigados
 -- limitando a quantidade de feedback em 3
 -- PODE INDICAR MÁ ESCRITA DO DESAFIO? OU ALGO MAIS A INVESTIGAR?
-select challenge_id, min(quantidade), max(quantidade), min(main_idea), max(theme)
+select WONT.challenge_id, WONT.wont_amount, WILL.will_amount, WONT.main_idea, WONT.theme
 from (
 	select cf.challenge_id as challenge_id, 
 		max(c.theme) as theme, 
-		cf.will_mitigate as will_mitigate,
-		count(cf.id) as quantidade,
+		count(cf.id) as wont_amount,
 		max(c.main_idea) as main_idea
 	from challenge_feedback cf, challenge c
 	where cf.challenge_id = c.id
-            and cf.user_id not in (1,3,23,25,25,26)
-	group by cf.challenge_id, cf.will_mitigate
+            and cf.will_mitigate = false
+            and cf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
+        group by cf.challenge_id
 	having count(cf.id) >= 3
-)
-group by challenge_id
-having count(challenge_id) = 2
-order by 1 
+) WONT
+JOIN (
+	select cf.challenge_id as challenge_id, 
+		max(c.theme) as theme, 
+		count(cf.id) as will_amount,
+		max(c.main_idea) as main_idea
+	from challenge_feedback cf, challenge c
+	where cf.challenge_id = c.id
+            and cf.will_mitigate = true
+            and cf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
+        group by cf.challenge_id
+	having count(cf.id) >= 3
+) WILL
+
+ON WONT.challenge_id= WILL.challenge_id
+
+ORDER BY 2 DESC, 3 DESC
 
 
 -- top 10 e total de challenges com mais respostas "vai mitigar", e "tem link com recomendação"
--- sql/feedbackTeachingMethod.sql
+++ sql/feedbackTeachingMethod.sql
@@ -7,44 +7,19 @@ from teaching_method_feedback f,
 	teaching_method c, users u
 where f.teaching_method_id = c.id
 and f.user_id = u.id
-and f.user_id not in (1,3,23,25,25,26)
+and f.user_id in (27,28,29,30,31,32,35,36,37,40,41)
 group by u.username
 order by 1, quantidade desc
 
 
--- listando a quantidade de feedback de teaching method com comentario "ja usaram"
--- agrupados por usuario e "ja usuram"
--- considerando apenas os usuarios participantes do estudo de caso
-select f.user_id, min(u.username), f.used_already,
-	count(distinct f.id) as quantidade_teaching_method_comment
-from teaching_method_feedback f, users u
-where length(f.used_already_comment) > 0 
-and f.user_id not in (1,3,23,25,25,26)
-and f.user_id = u.id
-group by f.user_id, f.used_already
-order by 2;
-
-
--- listando a quantidade de feedback de teaching method com comentario "pretende usar"
--- agrupados por usuario e "pretende usar"
--- considerando apenas os usuarios participantes do estudo de caso
-select f.user_id, min(u.username), f.will_use,
-	count(distinct f.id) as quantidade_teaching_method_comment
-from teaching_method_feedback f, users u
-where length(f.will_use_comment) > 0 
-and f.user_id not in (1,3,23,25,25,26)
-and f.user_id = u.id
-group by f.user_id, f.will_use
-order by 2;
-
 
 -- listando as teaching method mais comentados que FORAM utilizadas
 -- limitando a quantidade de feedback em 3
-select rf.teaching_method_id, count(rf.id) as quantidade, max(r.definition)
+select rf.teaching_method_id, count(rf.id) as quantidade, max(r.name)
 from teaching_method_feedback rf, teaching_method r
 where rf.teaching_method_id = r.id
-and rf.user_id not in (1,3,23,25,25,26)
 and rf.used_already = true
+and rf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
 group by rf.teaching_method_id
 having count(rf.id) >= 3
 order by quantidade desc
@@ -52,68 +27,84 @@ order by quantidade desc
 
 -- listando as teaching method mais comentados que NÃO FORAM utilizadas e PRETENDE utilizar
 -- limitando a quantidade de feedback em 3
-select rf.teaching_method_id, count(rf.id) as quantidade, max(r.definition)
+select rf.teaching_method_id, count(rf.id) as quantidade, max(r.name)
 from teaching_method_feedback rf, teaching_method r
 where rf.teaching_method_id = r.id
-and rf.user_id not in (1,3,23,25,25,26)
 and rf.used_already = false
 and rf.will_use = true
+and rf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
 group by rf.teaching_method_id
-having count(rf.id) >= 3
+having count(rf.id) >= 2
 order by quantidade desc
 
 
 -- listando as teaching method mais comentados que NÃO FORAM utilizadas e NÃO PRETENDE utilizar
 -- limitando a quantidade de feedback em 1
-select rf.teaching_method_id, count(rf.id) as quantidade, max(r.definition)
+select rf.teaching_method_id, count(rf.id) as quantidade, max(r.name)
 from teaching_method_feedback rf, teaching_method r
 where rf.teaching_method_id = r.id
-and rf.user_id not in (1,3,23,25,25,26)
 and rf.used_already = false
 and rf.will_use = false
+and rf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
 group by rf.teaching_method_id
 having count(rf.id) >= 1
 order by quantidade desc
 
 
 
--- listando as teaching method que ao mesmo tempo FORAM E NÃO FORAM UTILIZADAS
--- limitando a quantidade de feedback em 2
--- PODE INDICAR MÁ ESCRITA DA TEACHING METHOD? OU SERIA ALGO MAIS?
-select teaching_method_id, min(quantidade), max(quantidade), min(definition)
-from (
-	select rf.teaching_method_id as teaching_method_id, 
-		rf.used_already as used_already,
-		count(rf.id) as quantidade,
-		max(r.definition) as definition
-	from teaching_method_feedback rf, teaching_method r
-	where rf.teaching_method_id = r.id
-            and rf.user_id not in (1,3,23,25,25,26)
-	group by rf.teaching_method_id, rf.used_already
-	having count(rf.id) >= 2
-)
-group by teaching_method_id
-having count(teaching_method_id) = 2
-order by 1 
-
-
-
--- listando as teaching method que não foram utilizadas e ao mesmo tempo SERÃO E NÃO SERÃO UTILIZADAS
+-- listando as teaching methods que ao mesmo tempo FORAM + SERÃO COM NÃO SERÃO UTILIZADAS
 -- limitando a quantidade de feedback em 1
 -- PODE INDICAR MÁ ESCRITA DA RECOMENDAÇÃO? OU SERIA ALGO MAIS?
-select teaching_method_id, min(quantidade), max(quantidade), min(definition)
-from (
+SELECT WONT_USE.teaching_method_id, WONT_USE.wont_use_amount, USED_ALREADY_WILL_USE.used_already_will_use_amount, WONT_USE.name
+FROM 
+(
+    -- selecionando a quantidade de nao uso das recomendacoes
+    -- filtrando por 1
 	select rf.teaching_method_id as teaching_method_id, 
-		rf.will_use as will_use,
-		count(rf.id) as quantidade,
-		max(r.definition) as definition
+		count(rf.id) as wont_use_amount,
+		max(r.name) as name
 	from teaching_method_feedback rf, teaching_method r
 	where rf.teaching_method_id = r.id
-            and rf.user_id not in (1,3,23,25,25,26)
-            and rf.used_already = true
-	group by rf.teaching_method_id, rf.will_use
+			and rf.used_already = false
+			and rf.will_use = false
+            and rf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
+	group by rf.teaching_method_id
 	having count(rf.id) >= 1
-)
-group by teaching_method_id
-having count(teaching_method_id) = 2
-order by 1 
+) WONT_USE
+
+JOIN 
+(
+    -- selecionando a quantidade de recomendacoes ja usadas adicionadas com as que serao usadas
+    -- filtrando por 1
+    select coalesce(USED_ALREADY.teaching_method_id, WILL_USE.teaching_method_id) as teaching_method_id, 
+           coalesce(USED_ALREADY.used_already_amount,0) + coalesce(WILL_USE.will_use_amount, 0) as used_already_will_use_amount
+    from (
+        -- selecionando a quantidade de recomendacoes ja usadas
+        (select rf.teaching_method_id as teaching_method_id, 
+            count(rf.id) as used_already_amount
+        from teaching_method_feedback rf, teaching_method r
+        where rf.teaching_method_id = r.id
+                and rf.used_already = true
+                and rf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
+        group by rf.teaching_method_id) USED_ALREADY
+        
+        full join
+        
+        -- selecionando a quantidade de recomendacoes a serem usadas usadas
+        (select rf.teaching_method_id as teaching_method_id, 
+            count(rf.id) as will_use_amount
+        from teaching_method_feedback rf, teaching_method r
+        where rf.teaching_method_id = r.id
+                and rf.used_already = false
+                and rf.will_use = true
+                and rf.user_id in (27,28,29,30,31,32,35,36,37,40,41)
+        group by rf.teaching_method_id) WILL_USE
+        
+        on USED_ALREADY.teaching_method_id = WILL_USE.teaching_method_id
+    )
+    where coalesce(USED_ALREADY.used_already_amount,0) + coalesce(WILL_USE.will_use_amount, 0) >= 1
+) USED_ALREADY_WILL_USE
+
+ON WONT_USE.teaching_method_id= USED_ALREADY_WILL_USE.teaching_method_id
+
+ORDER BY 2 DESC, 3 DESC
